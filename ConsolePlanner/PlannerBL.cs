80067550e608b1a22bf17e788003c110d248f99f
Belinskiy21 <belinskiy21@gmail.com>
2017-01-07 10:43:31 +0200

    -Working files have been added. - -nothing changed.

---
+++ ConsolePlanner/PlannerBL.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace ConsolePlanner
+{
+    public interface IPlannerBl
+    {
+        void AddEvent();
+        void DeletEvent();
+        void ShowPlans();
+        
+    }
+   class PlannerBL : IPlannerBl
+    {
+        
+        private string Days { get; set; }
+
+        List<string> days = new List<string> { "ПН:", "ВТ:", "СР:", "ЧТ:", "ПТ:", "СБ:", "ВС:" };
+
+        public PlannerBL()
+        {
+            
+        }
+        public void AddEvent()
+        {
+                Console.WriteLine("Введите день события!(ПН,ВТ,СР,ЧТ,ПТ,СБ,ВС)\n  Для выхода в главное меню нажмите - E\n");
+
+                string d = Console.ReadLine();
+            switch (d)
+            {
+                case "ПН":
+                    Console.WriteLine("Введите текст события.");
+                    string p = Console.ReadLine();
+                    days.RemoveAt(0);
+                    days.Insert(0, p); 
+                    break;
+                case "ВТ":
+                    Console.WriteLine("Введите текст события");
+                    string v = Console.ReadLine();
+                    days.RemoveAt(1);
+                    days.Insert(1, v);
+                    break;
+                case "СР":
+                    Console.WriteLine("Введите текст события");
+                    string cp = Console.ReadLine();
+                    days.RemoveAt(2);
+                    days.Insert(2, cp);
+                    break;
+                case "ЧТ":
+                    Console.WriteLine("Введите текст события");
+                    string ch = Console.ReadLine();
+                    days.RemoveAt(3);
+                    days.Insert(3, ch);
+                    break;
+                case "ПТ":
+                    Console.WriteLine("Введите текст события");
+                    string pt = Console.ReadLine();
+                    days.RemoveAt(4);
+                    days.Insert(4, pt);
+                    break;
+                case "СБ":
+                    Console.WriteLine("Введите текст события");
+                    string s = Console.ReadLine();
+                    days.RemoveAt(5);
+                    days.Insert(5, s);
+                    break;
+                case "ВС":
+                    Console.WriteLine("Введите текст события");
+                    string vs = Console.ReadLine();
+                    days.RemoveAt(6);
+                    days.Insert(6, vs);
+                    break;
+                case "E":
+                    break;
+                }
+            
+            Console.Clear();
+            
+
+        }
+        public void DeletEvent()
+        {
+            Console.WriteLine("Введите день события,которое нужно удалить!(ПН,ВТ,СР,ЧТ,ПТ,СБ,ВС)\nДля выхода в главное меню нажмите - E\n");
+            string d = Console.ReadLine();
+            switch (d)
+            {
+                case "ПН":
+                    days.RemoveAt(0);
+                    break;
+                case "ВТ":
+                    days.RemoveAt(1);
+                    break;
+                case "СР":
+                    days.RemoveAt(2);
+                    break;
+                case "ЧТ":
+                    days.RemoveAt(3);
+                    break;
+                case "ПТ":
+                    days.RemoveAt(4);
+                    break;
+                case "СБ":
+                    days.RemoveAt(5);
+                    break;
+                case "ВС":
+                    days.RemoveAt(6);
+                    break;
+                case "E":
+                    break;          
+            }
+            
+        }
+        public void ShowPlans()
+        {
+            FileStream file = new FileStream("C:/Users/oleg/Documents/Visual Studio 2015/Projects/ FilePlanner.txt", FileMode.OpenOrCreate);
+            StreamWriter writer = new StreamWriter(file);
+            foreach (string el in days)
+            {
+                writer.WriteLine(el);
+                Console.WriteLine(el);
+            }
+            writer.Close();
+           
+        }
+    }
+}
---
+++ ConsolePlanner/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsolePlanner
+{
+    class Program
+    {
+        static void Main()
+        {
+
+
+
+            PlannerBL newevent = new PlannerBL();
+
+    Console.WriteLine(" Меню:\n Показать события: 1 \n Добавить событие: 2\n Удалить событие:  3\n Для выхода: 4\n");
+
+
+
+
+
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+                if (a == 1)
+                {
+                    newevent.ShowPlans();
+                }
+                else if (a == 2)
+                {
+                    newevent.AddEvent();
+                }
+                else if (a == 3)
+                {
+                    newevent.DeletEvent();
+                }
+                else if (a == 4)
+                {
+                    Console.WriteLine("Досвидания!");
+                    Console.ReadLine();
+                    
+            }
+              
+                else Console.WriteLine("Следуйте Указаниям Меню!");
+                Console.ReadLine();
+                Console.Clear();
+                Main();
+
+            Console.ReadKey();
+
+             
+        
+    }
+    }
+}
