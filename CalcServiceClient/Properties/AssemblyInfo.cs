b43b0b8b37f58b82c4b593a9e59d85bbc5e7f86f
Belinskiy21 <belinskiy21@gmail.com>
2017-01-15 20:40:17 +0200

    pending

--- CalcServiceClient/Properties/AssemblyInfo.cs
+++ CalcServiceClient/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CalculatorService1")]
+[assembly: AssemblyTitle("CalcServiceClient")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CalculatorService1")]
+[assembly: AssemblyProduct("CalcServiceClient")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("25eb7b37-bb6f-424b-abde-2ba3da6d367b")]
+[assembly: Guid("df9f6c97-db12-41b0-92a8-600d167c8ea0")]
 
 // Version information for an assembly consists of the following four values:
 //
